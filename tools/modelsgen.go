cff77befa18e93e9064d54b3d2a017aa329b7b1a
Guido M Mantilla T <guido.mantilla@yahoo.com>
2023-10-08 16:53:35 -0500

    update

--- tools/modelsgen.go
+++
@@ -1,3 +0,0 @@
-package tools
-
-//go:generate docker run --rm -v $PROJECT_HOME:/app/project --platform linux/amd64 gitea.linuxcode.net/linuxcode/dbml-go ./dbml-go-generator -f ./project/resources/dbml/database.dbml -p models -o ./project/pkg/models
