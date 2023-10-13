2a2a5800a43baabd45f79b509b7e5641a9d2b7a9
Guido M Mantilla T <guido.mantilla@yahoo.com>
2023-10-12 20:02:28 -0500

    update

--- pkg/models/types.go
+++ pkg/models/types.go
@@ -1,49 +1,38 @@
 package models
 
-type Model struct {
-	AuthResource          *AuthResource          `db_table:"auth_resources"`
-	AuthRole              *AuthRole              `db_table:"auth_roles"`
-	AuthAccessControlList *AuthAccessControlList `db_table:"auth_access_control_list"`
-	AuthUser              *AuthUser              `db_table:"auth_users"`
-	AuthPrincipal         *AuthPrincipal         `db_view:"auth_principals"`
-}
-
 type AuthResource struct {
-	ID      *int    `db_column:"id,pk"`
-	Name    *string `db_column:"name,uq"`
-	Enabled *bool   `db_column:"enabled"`
+	Name        *string `db:"name"`
+	Application *string `db:"application"`
+	Enabled     *bool   `db:"enabled"`
 }
 
 type AuthRole struct {
-	ID      *int    `db_column:"id,pk"`
-	Name    *string `db_column:"name,uq"`
-	Enabled *bool   `db_column:"enabled"`
+	Name    *string `db:"name"`
+	Enabled *bool   `db:"enabled"`
 }
 
 type AuthAccessControlList struct {
-	ID         *int    `db_column:"id,pk"`
-	RoleID     *int    `db_column:"role_id"`
-	ResourceID *int    `db_column:"resource_id"`
-	Permission *string `db_column:"permission"`
-	Enabled    *bool   `db_column:"enabled"`
+	Role       *string `db:"role"`
+	Resource   *string `db:"resource"`
+	Permission *string `db:"permission"`
+	Enabled    *bool   `db:"enabled"`
 }
 
 type AuthUser struct {
-	ID         *int    `db_column:"id,pk"`
-	RoleID     *int    `db_column:"role_id"`
-	Username   *string `db_column:"username,uq"`
-	Password   *string `db_column:"password"`
-	Passphrase *string `db_column:"passphrase"`
-	Enabled    *bool   `db_column:"enabled"`
+	Username   *string `db:"username"`
+	Role       *string `db:"role"`
+	Password   *string `db:"password"`
+	Passphrase *string `db:"passphrase"`
+	Enabled    *bool   `db:"enabled"`
 }
 
 type AuthPrincipal struct {
-	Username    *string `db_column:"username,uq"`
-	Role        *string `db_column:"role"`
-	Application *string `db_column:"application"`
-	Resource    *string `db_column:"resource"`
-	Permission  *string `db_column:"permission"`
-	Password    *string `db_column:"password"`
-	Passphrase  *string `db_column:"passphrase"`
-	Enabled     *bool   `db_column:"enabled"`
+	Username    *string `db:"username"`
+	Role        *string `db:"role"`
+	Application *string `db:"application"`
+	Resource    *string `db:"resource"`
+	Permission  *string `db:"permission"`
+	Password    *string `db:"password"`
+	Passphrase  *string `db:"passphrase"`
+	Enabled     *bool   `db:"enabled"`
 }
